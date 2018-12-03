f5477fcf8a9c93f1c38895c4541f138bfedec7d0
aman5prakash <amanprakash512@gmail.com>
2018-12-03 18:15:32 +0530

    added controller

---
+++ NotificationsController/NotificationsController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NotificationEngine.Services;
+using NotificationEngine.Models;
+using NotificationEngine.Migrations;
+using NotificationEngine.Hubs;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace Notifications.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        IReadNotificationService notificationObject;
+        public NotificationController(IReadNotificationService _notificationObject)
+        {
+            this.notificationObject = _notificationObject;
+        }
+
+        [HttpGet]
+        [Route("notifications/{UserId}")]
+        public async Task<IActionResult> GetNotifications(string UserId)
+        {
+            var notifications = await notificationObject.GetNotifications(UserId);
+            if (notifications != null)
+            {
+                return Ok(notifications);
+            }
+            else 
+            {
+                return NotFound();
+            }
+        }
+    }
+}
